ea88f924ed691977a0d9d6fab43ac8a480d6b3f7
Alex M <alexm0717@hotmail.com>
2023-02-22 18:10:53 -0800

    add model spec

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :address
+
   has_many :subscriptions
   has_many :teas, through: :subscription_teas
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,12 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+  validates_presence_of :customer_id
+  validates_presence_of :tea_id
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :status
+  validates_presence_of :frequency
+
+
 end
